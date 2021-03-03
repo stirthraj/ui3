e7c60f32ed916892423c55f0978c7a3bd7cac2e7
Tirth Raj Kumar <stirthraj.12345@gmail.com>
2021-03-03 19:33:34 +0530

    dropdown and no of items

---
+++ src/components/DataAll.js
@@ -0,0 +1,92 @@
+import axios from 'axios';
+import React, { Component } from 'react'
+
+export default class DataAll extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: [] };
+  }
+
+  componentWillUpdate() {
+    if (this.props.apitype === "small") {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json"
+        )
+        .then((res) => {
+          // console.log(res.data.length);
+
+          const users = res.data;
+          const len = users.length;
+          this.setState({ users, len });
+        });
+    } else if (this.props.apitype === "medium") {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/medium.json"
+        )
+        .then((res) => {
+          // console.log(res.data.length);
+
+          const users = res.data;
+          const len = users.length;
+          this.setState({ users, len });
+        });
+    } else {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json"
+        )
+        .then((res) => {
+          // console.log(res.data.length);
+
+          const users = res.data;
+          const len = users.length;
+          this.setState({ users, len });
+        });
+    }
+  }
+
+  render() {
+    const noOfData = this.props.noOfData;
+    return (
+      <div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Date</th>
+                <th>Salary</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.slice(0, noOfData).map((user) => (
+                <tr key={user.id}>
+                  <td>{user.firstName + user.lastName}</td>
+                  <td>{user.location}</td>
+                  <td>{user.date}</td>
+                  <td>{user.salary}</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Date</th>
+                <th>Salary</th>
+              </tr>
+            </tfoot>
+          </table>
+          <div>
+            <h6>
+              Showing 1 to {noOfData} of {this.state.len} entries
+            </h6>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/DataList.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import DataAll from "./DataAll";
+
+export default class DataList extends Component {
+    constructor(props){ 
+        super(props);
+        this.state={noOfData:'',value:''}
+
+    }
+
+
+  onChangeHandle = (event) => {
+    this.setState({ noOfData: event.target.value });
+  };
+  onChangeHandleApi = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  
+  render() {
+      
+      let noOfData=this.state.noOfData===''?10:this.state.noOfData;
+      let value=this.state.value===''?"medium":this.state.value;
+
+
+    return (
+      <div>
+        <div className="flex">
+          <div>
+            Show <input type="number" value={noOfData} onChange={this.onChangeHandle} />
+            Entries
+          </div>
+          <div>
+            <label>
+              Pick Api type:{this.state.value}
+              <select value={value} onChange={this.onChangeHandleApi}>
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+              </select>
+            </label>
+          </div>
+        </div>
+        <DataAll noOfData={noOfData} apitype={value} />
+      </div>
+    );
+  }
+}
